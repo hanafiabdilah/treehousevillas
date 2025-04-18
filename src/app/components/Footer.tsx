99dd3d387c671ddbbfcfd9f96a5db9ee18f14294
Hanafi Abdilah <handilah81@gmail.com>
2025-04-18 23:49:59 +0700

    footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+  export default function Footer() {
+    return (
+      <footer className="bg-white border-t border-gray-200">
+          <p className="my-8 text-center text-sm/6 text-gray-600">
+            &copy; 2025 Tree House Villas, Inc. All rights reserved.
+          </p>
+      </footer>
+    )
+  }
+  
