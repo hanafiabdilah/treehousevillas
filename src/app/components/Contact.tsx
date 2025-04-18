88ef35ba399cfbefe6ce0206080003dd95c831b2
Hanafi Abdilah <handilah81@gmail.com>
2025-04-18 23:55:21 +0700

    ok

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export default function Contact() {
   return (
